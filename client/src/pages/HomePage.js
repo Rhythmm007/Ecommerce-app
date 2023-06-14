84c21208619473d216aa75806e3868fe70ad1165
Rhythm01~ <robiulhaque1919@gmail.com>
2023-06-15 04:02:41 +0600

    initial commit

---
+++ client/src/pages/HomePage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Layout from '../components/Layout/Layout';
+
+const HomePage = () => {
+    return (
+        <Layout>
+            Home
+        </Layout>
+    );
+};
+
+export default HomePage;
